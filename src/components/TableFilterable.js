21e0ce19ac3eacfc84147d126143e53357756d7f
Nikola Horvat <nikola.horvat@amodo.eu>
2017-04-11 18:11:17 +0200

    Add filter checkboxes to employees table hrader

--- src/components/TableFilterable.js
+++ src/components/TableFilterable.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-import { Table } from 'semantic-ui-react';
+import { Table, Checkbox } from 'semantic-ui-react';
 import '../css/components/TableFilterable.css';
 
 class TableFilterable extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      filters: null
+    }
   }
 
   render() {
     let { employees } = this.props.data;
 
-    let rows =  employees.map((obj, i) => {
+    let columns = (
+      <Table.Row>
+        <Table.HeaderCell></Table.HeaderCell>
+        <Table.HeaderCell>Name</Table.HeaderCell>
+        <Table.HeaderCell>Bio</Table.HeaderCell>
+        <Table.HeaderCell>Role</Table.HeaderCell>
+        <Table.HeaderCell>Skills</Table.HeaderCell>
+      </Table.Row>
+    );
+
+    let checkboxes = [];
+
+    let rows = employees.map((obj, i) => {
       let { name, bio, profileImage, role, skills } = obj;
+
+      skills.forEach(skill => {
+        if (!checkboxes.includes(skill)) {
+          checkboxes.push(skill);
+        }
+      });
 
       return (
         <Table.Row key={i}>
@@ -29,19 +49,19 @@ class TableFilterable extends Component {
       );
     });
 
+    let filters = (
+      <div className="center-block text-center">
+        { checkboxes.map((c, i) => <Checkbox label={c} key={i} />)}
+      </div>
+    );
+
     return (
       <Table className='TableFilterable'>
         <Table.Header>
           <Table.Row>
-            <Table.HeaderCell colSpan='3'>Employees</Table.HeaderCell>
+            <Table.HeaderCell colSpan='5'>{ filters }</Table.HeaderCell>
           </Table.Row>
-          <Table.Row>
-            <Table.HeaderCell></Table.HeaderCell>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Bio</Table.HeaderCell>
-            <Table.HeaderCell>Role</Table.HeaderCell>
-            <Table.HeaderCell>Skills</Table.HeaderCell>
-          </Table.Row>
+          { columns }
         </Table.Header>
         <Table.Body>
           { rows }
