9f391f2b0a17644d57cb6cb28b7e50ea405865a5
Nikola Horvat <nikola.horvat@amodo.eu>
2017-04-11 12:15:38 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>OpenWeb assignment</h2>
+          <p className="App-intro">
+            An overview of the employees that work at Open Web.
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
