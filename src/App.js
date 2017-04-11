14903c75b9fd41715c007fa3d3a798d569a29877
Nikola Horvat <nikola.horvat@amodo.eu>
2017-04-11 13:47:38 +0200

    Add Bootstrap3 library

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import classnames from 'classnames';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>OpenWeb assignment</h2>
-          <p className="App-intro">
+      <div className={classnames('App', 'container-fluid')}>
+        <div className={classnames('page-header', 'text-center')}>
+          <h3>OpenWeb assignment</h3>
+          <h4 className={classnames('text-info')}>
             An overview of the employees that work at Open Web.
-          </p>
+          </h4>
+        </div>
+        <div className={classnames('content')}>
+          <table className="table"></table>
         </div>
       </div>
     );
